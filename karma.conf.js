46ab4f27e9f2dda382bafbad57d1aa7464101c4c
Jongmoon Yoon <jongmoon.yoon@navercorp.com>
2017-01-12 13:08:11 +0900

    test: add coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,7 @@
 module.exports = function(config) {
-	config.set({
+	var karmaConfig = {
+		reporters:[],
+		browsers: [],
 		frameworks: ['qunit'],
 		files: [
 			// vendor files
@@ -23,9 +25,32 @@ module.exports = function(config) {
 		// // preprocess matching files before serving them to the browser
     // // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/unit/**/*.js': ['webpack']
-    },
-		browsers: ["PhantomJS"],
-		singleRun: false
-	});
+      'test/unit/**/*.js': config.coverage ? ['webpack'] : ['webpack', 'sourcemap']
+    }
+	};
+
+	if(config.coverage) {
+    karmaConfig.reporters.push("coverage");
+    karmaConfig.coverageReporter = {
+        type: 'html',
+        dir: 'coverage'
+    };
+    karmaConfig.browsers.push("PhantomJS");
+    karmaConfig.webpack.module.rules.push(
+      {
+        test: /(\.js)$/,
+        exclude: /(test|node_modules)/,
+        enforce: "pre",
+        loader: 'isparta-loader'
+      }
+    );
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    karmaConfig.singleRun = true;
+  } else {
+    karmaConfig.browsers.push("Chrome");
+    karmaConfig.singleRun = false;
+  }
+
+  config.set(karmaConfig);
 };
