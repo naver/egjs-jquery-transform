26eb1063b84a9a84acba97fb93fefb4dd5579ac3
Jongmoon Yoon <jongmoon.yoon@navercorp.com>
2017-04-05 20:02:21 +0900

    docs(transform): solve not generating document

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import {rateFn} from "./transform";
 
 const $ = jQuery;
 
+/**
+ * @namespace jQuery
+ */
 if (!$) {
 	console.warn("jQuery is not defined.");
 } else {
@@ -10,8 +13,8 @@ if (!$) {
 	 * A method that extends the <a href=http://api.jquery.com/animate/>.animate()</a> method provided by jQuery. With this method, you can use the transform property and 3D acceleration
 	 * @ko jQuery의<a href=http://api.jquery.com/animate/>animate() 메서드</a>를 확장한 메서드. CSS의 transform 속성과 3D 가속을 사용할 수 있다.
 	 * @name jQuery#animate
+	 * @alias eg.Transform
 	 * @method
-	 * @alias eg.transform
 	 * @param {Object} properties An object composed of the CSS property and value which will be applied to an animation<ko>애니메이션을 적용할 CSS 속성과 값으로 구성된 객체</ko>
 	 * @param {Number|String} [duration=4000] Duration of the animation (unit: ms)<ko>애니메이션 진행 시간(단위: ms)</ko>
 	 * @param {String} [easing="swing"] The easing function to apply to an animation<ko>애니메이션에 적용할 easing 함수</ko>
